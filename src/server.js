2235e37530db1ce6ac1d95ae7d15ff7536c2bd4c
Akshat Singh <akshat.singh11123@gmail.com>
2026-08-06 00:34:00 +0530

    Port Error Fixed

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const connectDatabase = require("./config/database");
 
 const app = express();
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 5080;
 
 // Basic server middleware only.
 app.use(express.json());
